127781e8580ff15f4b8cce2aa705334d8b0db6eb
amule <Anand.Mule@elliemae.com>
2019-04-26 11:42:12 +0530

    jenkins

---
+++ JenkinsAnand/src/main/java/jenkinsAnand/JenkinsAnand/App.java
@@ -0,0 +1,18 @@
+package jenkinsAnand.JenkinsAnand;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	@Test
+    public  void test(){
+    	
+    
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ JenkinsAnand/src/test/java/jenkinsAnand/Helloworld.java
@@ -0,0 +1,13 @@
+package jenkinsAnand;
+
+
+
+class Helloworld{
+	
+	
+	@org.testng.annotations.Test
+	public void Test()
+	{
+		System.out.println("Hello word");
+	}
+}
